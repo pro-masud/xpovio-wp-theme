7d713a3850ba86377c8c45a65e5740a78b72a67e
pro-masud <promasudbd@gmail.com>
2024-03-02 19:18:10 +0600

    mongodb database data insert and show single data, get all data show and delete single data

--- controller/createUserMiddleware.js
+++ controller/createUserMiddleware.js
@@ -1,7 +1,7 @@
 import Customer from "../models/product.js";
 
 export const createUserMiddleware = async (req, res) => {
-    const {name, age, location, gender, cell, status} = req.body;
+    const {name, age, location, gender, cell, email, status} = req.body;
 
     // file manage here
     let userPhoto = null;
@@ -15,9 +15,35 @@ export const createUserMiddleware = async (req, res) => {
         location,
         gender,
         cell,
+        email,
         userPhoto,
         status,
     });
 
     res.status(201).json(data);
+}
+
+
+// get all user here
+export const getUserMiddleware = async (req, res) => {
+    const data = await Customer.find();
+    console.log(data);
+    res.status(200).json(data);
+}
+
+
+// get single product here now
+export const getSinglePost = async (req, res) => {
+    const { id } = req.params;
+    const singleData = await Customer.findOne({_id: id});
+
+    res.status(200).json(singleData);
+}
+
+// get single product here now
+export const deleteSinglePost = async (req, res) => {
+    const { id } = req.params;
+    const deleteData = await Customer.deleteOne({_id: id});
+
+    res.status(200).json(deleteData);
 }
--- router/user.js
+++ router/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUserMiddleware } from "../controller/createUserMiddleware.js";
+import { createUserMiddleware, deleteSinglePost, getSinglePost, getUserMiddleware } from "../controller/createUserMiddleware.js";
 import { userImageFileUpload } from "../util/multer.js";
 
 
@@ -8,6 +8,9 @@ import { userImageFileUpload } from "../util/multer.js";
 const router = express.Router();
 
 router.post("/user", userImageFileUpload, createUserMiddleware);
+router.get("/user", getUserMiddleware);
+router.get("/user/:id", getSinglePost);
+router.delete("/user/:id", deleteSinglePost);
 
 // export default router
 export default router;
